470308334aca3a3526a73209b7cd4dbbe90c6c67
hyunho <gusgh3315@gmail.com>
2021-09-25 15:15:15 +0900

    to 5-9

--- quiz/05-1/sample/js/app.js
+++ quiz/05-1/sample/js/app.js
@@ -4,20 +4,27 @@ var app = new Vue({
     message : 'Hello Vue.js',
     // 할 일 #1
     // 새로운 데이터 속성을 1개 추가하고, 머스타쉬 {{ }} 데이터 바인딩을 이용하여 화면에 표시해보세요.
+    todo : 'haircut',
 
     uid: '10',
     // 할 일 #2
     // uid를 변경하고 해당 uid의 변경 여부를 크롬 개발자 도구의 '화면 요소 검사 기능'으로 확인해보세요. p 태그의 id 값을 검사.
 
-    flag: true
+    flag: true,
     // 할 일 #4
     // 위 flag 값을 false로 변경했을 때 화면에 어떤 영향을 주는지 확인해보세요.
+    cnt:0
   },
   methods: {
     // ES6 문법
     clickBtn() {
       console.log("hi");
     },
+    countUp: function(){
+      this.cnt+=1;
+      console.log(this.cnt);
+      return this.cnt;
+    }
     // ES5 문법 - 위 ES6 문법과 동일한 코드
     // clickBtn: function() {
     //   console.log("hi");
